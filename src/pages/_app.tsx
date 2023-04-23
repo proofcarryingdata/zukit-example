5d269bd9457dd7ee00629404c705e3357475dfb3
DC <dcposch@dcpos.ch>
2023-04-23 13:42:13 +0200

    Zukit example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ZupassProvider } from "zukit";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ZupassProvider>
+      <Component {...pageProps} />
+    </ZupassProvider>
+  );
 }
