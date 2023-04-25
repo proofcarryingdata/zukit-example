2bc99cbbcae59458d54061b47a3dd0e747f8f83b
DC <dcposch@dcpos.ch>
2023-04-26 01:12:15 +0200

    Upgrade zukit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { ZupassProvider } from "zukit";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ZupassProvider>
+    <ZupassProvider passportClientURL="http://localhost:3000">
       <Component {...pageProps} />
     </ZupassProvider>
   );
